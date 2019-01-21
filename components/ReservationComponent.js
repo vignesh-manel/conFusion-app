e4043d4db5f3a4b7d5551dfea1633cdc5e552a9c
vmanel <vnayak83@gmail.com>
2019-01-21 17:39:58 +0000

    Modals

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, Picker, Button, StyleSheet, Switch } from 'react-native';
+import { Text, View, ScrollView, Picker, Button, StyleSheet, Switch, Modal } from 'react-native';
 import { Card } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
 
@@ -10,7 +10,8 @@ class Reservation extends Component {
 	this.state = {
 	    guests: 1,
 	    smoking: false,
-	    date:''
+	    date:'',
+	    showModal: false
 	}
     }
 
@@ -18,8 +19,15 @@ class Reservation extends Component {
 	title: 'Reserve Table'
     }
 
+    toggleModal() {
+	this.setState({showModal: !this.state.showModal })
+    }
+
     handleReservation() {
-	alert(JSON.stringify(this.state))
+	this.toggleModal();
+    }
+
+    resetForm() {
 	this.setState({
 	    guests: 1,
 	    smoking: false,
@@ -90,6 +98,23 @@ class Reservation extends Component {
 			onPress={() => this.handleReservation()}
 			accesbilityLabel='Learn more about this purple button' />
 		</View>
+		<Modal
+		    animationType={'slide'}
+		    transparent={false}
+		    visible={this.state.showModal}
+		    onDismiss={() => {this.toggleModal(); this.resetForm()}}
+		    onRequestClose={() => {this.toggleModal(); this.resetForm()}}>
+		    <View style={styles.modal}>
+			<Text style={styles.modalTitle}>Your Reservation</Text>
+			<Text style={styles.modalText}>Number of Guests: {this.state.guests}</Text>
+			<Text style={styles.modalText}>Smoking?: {this.state.smoking ? 'Yes': 'No'}</Text>
+			<Text style={styles.modalText}>Date and Time: {this.state.date}</Text>
+			<Button
+			    onPress={() => {this.toggleModal(); this.resetForm()}}
+			    color='#512DA8'
+			    title='Close'></Button>
+		    </View>
+		</Modal>
 	    </ScrollView>
 	);
     }
@@ -110,6 +135,22 @@ const styles = StyleSheet.create({
     },
     formItem: {
 	flex: 1
+    },
+    modal: {
+	justifyContent: 'center',
+	margin: 20
+    },
+    modalTitle: {
+	fontSize: 24,
+	fontWeight: 'bold',
+	backgroundColor: '#512DA8',
+	textAlign: 'center',
+	color: 'white',
+	marginBottom: 20
+    },
+    modalText: {
+	fontSize: 18,
+	margin: 10
     }
 })
 
