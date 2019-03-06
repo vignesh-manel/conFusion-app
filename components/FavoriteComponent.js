bc30d3ea9cc333d8485112141dae36b801bff952
vmanel <vnayak83@gmail.com>
2019-03-06 17:21:27 +0000

    Revert "Alert and Animation"
    This reverts commit 7f951634e634c3d5a784686e0dc7c699f9df3c22.

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image,View,FlatList,Alert } from 'react-native';
+import { Image,View,FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -33,24 +33,7 @@ class Favorites extends Component {
 	    {
 		text:'Delete',
 		type:'delete',
-		onPress: () => {
-				Alert.alert(
-				    'Delete Favorite',
-				    'Are you sure you wish to delete favorite dish '+item.name+'?',
-				    [
-					{
-					    text:'Cancel',
-					    onPress: () => console.log(item.name+' Not deleted'),
-					    style:'cancel'
-					},
-					{
-					    text:'OK',
-					    onPress: () => this.props.deleteFavorite(item.id)
-					}
-				    ],
-				    { cancelable:false }
-				)
-				}
+		onPress: () => this.props.deleteFavorite(item.id)
 	    }
 	];
 
